9e862930ee95715bbd1aabca58066df154e74bdc
Schparky <3172830+Schparky@users.noreply.github.com>
2020-12-04 13:04:02 -0700

    correct list notation

--- main.tf
+++ main.tf
@@ -82,15 +82,7 @@ module "ecs-service-cloudwatch-dashboard" {
  */
 resource "aws_elasticache_subnet_group" "memcache_subnet_group" {
   name = "${var.app_name}-${data.terraform_remote_state.common.outputs.app_env}"
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibility in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
-  subnet_ids = [data.terraform_remote_state.common.outputs.private_subnet_ids]
+  subnet_ids = data.terraform_remote_state.common.outputs.private_subnet_ids
 }
 
 /*
@@ -103,14 +95,6 @@ resource "aws_elasticache_cluster" "memcache" {
   port                 = var.memcache_port
   num_cache_nodes      = var.memcache_num_cache_nodes
   parameter_group_name = var.memcache_parameter_group_name
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibility in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
   security_group_ids = [data.terraform_remote_state.common.outputs.vpc_default_sg_id]
   subnet_group_name  = aws_elasticache_subnet_group.memcache_subnet_group.name
   az_mode            = var.memcache_az_mode
