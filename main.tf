32f860cf5172e5c3f8ecb7ef40a1804d238e9aeb
briskt <3172830+briskt@users.noreply.github.com>
2023-06-06 00:02:02 +0800

    create a deployment user

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ module "app" {
   domain_name              = var.cloudflare_domain
   container_def_json       = data.template_file.task_def_hub.rendered
   create_dns_record        = var.create_dns_record
+  create_cd_user           = true
   database_name            = local.mysql_database
   database_user            = local.mysql_user
   desired_count            = var.desired_count
