b4e8ad72b64ee60380c78f3499180b12d9997a10
briskt <3172830+briskt@users.noreply.github.com>
2023-06-09 14:17:50 +0800

    add documentation

--- vars.tf
+++ vars.tf
@@ -1,17 +1,7 @@
-variable "admin_email" {
-  description = "SAML technical contact email. This information will be available in the generated metadata."
-  type        = string
-}
 
-variable "admin_name" {
-  description = "SAML technical contact name. This information will be available in the generated metadata."
-  type        = string
-}
-
-variable "analytics_id" {
-  description = "Google Analytics measurement ID"
-  type        = string
-}
+/*
+ * General config
+ */
 
 variable "app_name" {
   description = "A name to be used, combined with \"app_env\", for naming resources. Should be unique in the AWS account."
@@ -28,9 +18,23 @@ variable "app_environment" {
   type        = string
 }
 
+variable "customer" {
+  description = "Customer name, used in AWS tags"
+  type        = string
+}
+
+
+/*
+ * AWS configuration
+ */
+
 variable "aws_access_key_id" {
   description = ""
   default     = null
+}
+
+variable "aws_secret_access_key" {
+  default = null
 }
 
 variable "aws_region" {
@@ -43,43 +47,40 @@ variable "aws_region_secondary" {
   default     = ""
 }
 
-variable "aws_secret_access_key" {
-  default = null
-}
-
-variable "cloudflare_domain" {
-  description = "The domain name on which to host the app. Combined with \"subdomain\" to create an ALB listener rule. Also used for the optional DNS record."
-}
-
-variable "cloudflare_token" {
-  description = "The Cloudflare API token with permissions on `cloudflare_domain`."
-  default     = null
-}
-
-variable "cpu" {
-  description = "The hard limit of CPU units to present for the task, expressed as an integer using CPU units, e.g. 512 = 0.5 vCPU"
-  default     = "128"
-}
-
-variable "create_dns_record" {
-  description = "Set to false to skip creation of a Cloudflare record"
-  default     = true
-}
-
-variable "desired_count" {
-  description = "Number of tasks to place and keep running."
-  default     = 2
-}
-
 variable "docker_tag" {
   description = "Docker tag to use in the task definition. Must match the tag name defined in the instance repo's `push_latest` step."
   default     = "latest"
 }
 
-variable "help_center_url" {
-  description = "The URL for the \"Help\" link at the top of the IDP selection page"
+variable "create_dashboard" {
+  description = "Set to true to create a CloudWatch dashboard"
+  type        = bool
+  default     = true
+}
+
+
+/*
+ * Task definition configuration
+ */
+
+variable "admin_email" {
+  description = "SAML technical contact email. This information will be available in the generated metadata."
   type        = string
-  default     = ""
+}
+
+variable "admin_name" {
+  description = "SAML technical contact name. This information will be available in the generated metadata."
+  type        = string
+}
+
+variable "analytics_id" {
+  description = "Google Analytics measurement ID"
+  type        = string
+}
+
+variable "cpu" {
+  description = "The hard limit of CPU units to present for the task, expressed as an integer using CPU units, e.g. 512 = 0.5 vCPU"
+  default     = "128"
 }
 
 variable "idp_display_name" {
@@ -90,6 +91,12 @@ variable "idp_name" {
   description = ""
 }
 
+variable "help_center_url" {
+  description = "The URL for the \"Help\" link at the top of the IDP selection page"
+  type        = string
+  default     = ""
+}
+
 variable "memory" {
   description = "The hard limit of memory (in MiB) to present to the task, expressed as an integer"
   default     = "128"
@@ -97,24 +104,32 @@ variable "memory" {
 
 variable "show_saml_errors" {
   description = "Used for SimpleSAMLphp `showerrors` config option. When enabled, all error messages and stack traces will be output to the browser."
+  type        = string
   default     = "false"
 }
 
-variable "subdomain" {
-  description = "The subdomain on which to host the app. Combined with \"domain_name\" to create an ALB listener rule. Also used for the optional DNS record."
+
+/*
+ * DNS configuration
+ */
+
+variable "cloudflare_domain" {
+  description = "The domain name on which to host the app. Combined with \"subdomain\" to create an ALB listener rule. Also used for the optional DNS record."
 }
 
-variable "customer" {
-  description = "Customer name, used in AWS tags"
-  type        = string
+variable "cloudflare_token" {
+  description = "The Cloudflare API token with permissions on `cloudflare_domain`."
+  default     = null
 }
 
-variable "create_dashboard" {
-  description = "Set to true to create a CloudWatch dashboard"
-  type        = bool
+variable "create_dns_record" {
+  description = "Set to false to skip creation of a Cloudflare record"
   default     = true
 }
 
+variable "subdomain" {
+  description = "The subdomain on which to host the app. Combined with \"cloudflare_domain\" to create an ALB listener rule. Also used for the optional DNS record."
+}
 
 /*
  * ECS and ASG configuration
@@ -135,10 +150,16 @@ variable "alarm_actions_enabled" {
   default     = false
 }
 
+variable "desired_count" {
+  description = "Number of tasks to place and keep running."
+  default     = 2
+}
+
 variable "ssh_key_name" {
   description = "Name of SSH key pair to use as default (ec2-user) user key. Set in the launch template"
   default     = ""
 }
+
 
 /*
  * VPC configuration
