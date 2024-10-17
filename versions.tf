1a81be251bb7f4eddb48f8be42063d3aaee835b2
briskt <3172830+briskt@users.noreply.github.com>
2024-10-17 10:34:06 +0800

    upgrade aws and cloudflare providers

--- versions.tf
+++ versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_version = ">= 0.14"
   required_providers {
     aws = {
-      version = "~> 4.0"
+      version = ">= 4.0.0, < 6.0.0"
       source  = "hashicorp/aws"
     }
     cloudflare = {
-      version = "~> 3.7"
+      version = ">= 3.7.0, < 5.0.0"
       source  = "cloudflare/cloudflare"
     }
     random = {
