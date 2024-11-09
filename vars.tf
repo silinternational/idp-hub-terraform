d41267dfbaaba41b51bea0a2ed677a86d96ee490
briskt <3172830+briskt@users.noreply.github.com>
2024-11-09 09:34:00 +0800

    add "(case-sensitive)" note to logging_level description

--- vars.tf
+++ vars.tf
@@ -107,7 +107,7 @@ variable "help_center_url" {
 }
 
 variable "logging_level" {
-  description = "Log level for log filter, may be one of: ERR, WARNING, NOTICE, INFO, DEBUG"
+  description = "Log level for log filter, may be one of: ERR, WARNING, NOTICE, INFO, DEBUG (case-sensitive)"
   type        = string
   default     = "NOTICE"
 }
